3bba83dc4d964ce20bafb93e97264114f6a8bafc
lucianorviana <lribeiroviana98@gmail.com>
2023-01-13 09:47:06 -0300

    style:documentos acrescentados
    documentos variados
         README.md
         index.html
         raiz_quadrada.py

---
+++ raiz_quadrada.py
@@ -0,0 +1,25 @@
+import math
+num=float(input('digite um valor  '))
+print(num)
+raiz =math.sqrt(num)
+print('A raiz de {} é igual a {:.2f} ' .  format(num,raiz)) 
+
+
+
+num=int(input('digite um valor  '))
+raiz=num**(1/2)
+print('A raiz de {} é igual a {}' . format(num,raiz))
+
+
+import math
+num=float(input('digite um valor  '))
+raiz=math.pow(num,1/2)
+print('A raiz de {} é igual a {} '  . format(num ,raiz))
+
+
+print(40 **(1/2))
+
+
+#raiz quadrada de 4 = 2² porque 2x2=4
+#raiz quadrada de 9 = 3² porque 3x3=9
+
